a1269de094085dce66897b72e9d086f45ee3bf1d
shuhanmirza <shuhan.mirza@gmail.com>
2022-11-03 17:05:32 +0600

    add vuetify and router

---
+++ foobar-vue/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="input-div">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
+
+<style scoped>
+
+</style>
