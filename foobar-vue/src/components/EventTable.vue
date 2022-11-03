4e9aafdb029c883e3daf67385536fa1db0cbee0c
shuhanmirza <shuhan.mirza@gmail.com>
2022-11-03 19:19:20 +0600

    design event table header

--- foobar-vue/src/components/EventTable.vue
+++ foobar-vue/src/components/EventTable.vue
@@ -3,128 +3,118 @@
     <v-data-table
         :headers="headers"
         :items="eventList"
-        sort-by="calories"
-        class="elevation-1"
+        class="elevation-21"
+        style="border-radius: 16px"
     >
+
       <template v-slot:top>
-        <v-toolbar
-            flat
+        <v-card flat style="border-radius: 16px; margin-bottom: -10px">
+          <v-card-title color="grey darken-2 white--text">
+            {{ tableTitle }}
+          </v-card-title>
+          <v-card-text style="margin-top: -12px">
+            List of Events! - <span class="text-button" @click="createItem()"> Create </span>
+          </v-card-text>
+        </v-card>
+
+        <v-divider/>
+
+        <v-dialog
+            v-model="createEventDialog"
+            max-width="500px"
         >
-          <v-toolbar-title>My CRUD</v-toolbar-title>
-          <v-divider
-              class="mx-4"
-              inset
-              vertical
-          ></v-divider>
-          <v-spacer></v-spacer>
-          <v-dialog
-              v-model="dialog"
-              max-width="500px"
-          >
-            <template v-slot:activator="{ on, attrs }">
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                  >
+                    <v-text-field
+                        v-model="editedItem.name"
+                        label="Dessert name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                  >
+                    <v-text-field
+                        v-model="editedItem.calories"
+                        label="Calories"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                  >
+                    <v-text-field
+                        v-model="editedItem.fat"
+                        label="Fat (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                  >
+                    <v-text-field
+                        v-model="editedItem.carbs"
+                        label="Carbs (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                  >
+                    <v-text-field
+                        v-model="editedItem.protein"
+                        label="Protein (g)"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
               <v-btn
-                  color="primary"
-                  dark
-                  class="mb-2"
-                  v-bind="attrs"
-                  v-on="on"
+                  color="blue darken-1"
+                  text
+                  @click="close"
               >
-                New Item
+                Cancel
               </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
-
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                    >
-                      <v-text-field
-                          v-model="editedItem.name"
-                          label="Dessert name"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                    >
-                      <v-text-field
-                          v-model="editedItem.calories"
-                          label="Calories"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                    >
-                      <v-text-field
-                          v-model="editedItem.fat"
-                          label="Fat (g)"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                    >
-                      <v-text-field
-                          v-model="editedItem.carbs"
-                          label="Carbs (g)"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                    >
-                      <v-text-field
-                          v-model="editedItem.protein"
-                          label="Protein (g)"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="close"
-                >
-                  Cancel
-                </v-btn>
-                <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="save"
-                >
-                  Save
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
+              <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="save"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </template>
       <template v-slot:[`item.actions`]="{ item }">
         <v-icon
@@ -157,7 +147,8 @@
 export default {
   name: "EventTable",
   data: () => ({
-    dialog: false,
+    tableTitle: "Events",
+    createEventDialog: false,
     dialogDelete: false,
     headers: [
       {text: 'Name', align: 'start', sortable: false, value: 'name'},
@@ -208,10 +199,13 @@ export default {
       ]
     },
 
+    createItem() {
+      this.createEventDialog = true
+    },
     editItem(item) {
       this.editedIndex = this.eventList.indexOf(item)
       this.editedItem = Object.assign({}, item)
-      this.dialog = true
+      this.createEventDialog = true
     },
 
     deleteItem(item) {
@@ -226,7 +220,7 @@ export default {
     },
 
     close() {
-      this.dialog = false
+      this.createEventDialog = false
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
@@ -281,5 +275,9 @@ export default {
 
 
 <style scoped>
-
+.text-button {
+  color: blue;
+  font-weight: bolder;
+  text-decoration: underline ;
+}
 </style>
