f70626de4ac6ddf1991a191ffe21a8df938294b3
shuhanmirza <shuhan.mirza@gmail.com>
2022-11-04 14:41:32 +0600

    dockerize whole migration process

--- foobar-go/db/migration/000002_init_seeds.down.sql
+++ foobar-go/db/migration/000002_init_seeds.down.sql
@@ -1,3 +1,8 @@
+DELETE FROM "events" WHERE "name"='test event 1"';
+DELETE FROM "events" WHERE "name"='test event 2"';
+DELETE FROM "events" WHERE "name"='test event 3"';
+DELETE FROM "events" WHERE "name"='test event 4"';
+
 DELETE FROM "locations" WHERE "name"='Bangladesh';
 DELETE FROM "locations" WHERE "name"='United Kingdom';
 DELETE FROM "locations" WHERE "name"='United States';
--- foobar-go/db/migration/000002_init_seeds.up.sql
+++ foobar-go/db/migration/000002_init_seeds.up.sql
@@ -4,3 +4,10 @@ INSERT INTO "locations" ("name") VALUES ('United States');
 INSERT INTO "locations" ("name") VALUES ('India');
 INSERT INTO "locations" ("name") VALUES ('Netherlands');
 INSERT INTO "locations" ("name") VALUES ('Russia');
+
+
+INSERT INTO "events" ("name", location_id, "datetime") VALUES ('test event 1', 1, 1667549960);
+INSERT INTO "events" ("name", location_id, "datetime") VALUES ('test event 2', 2, 1667649960);
+INSERT INTO "events" ("name", location_id, "datetime") VALUES ('test event 3', 3, 1667749960);
+INSERT INTO "events" ("name", location_id, "datetime") VALUES ('test event 4', 4, 1667839960);
+
